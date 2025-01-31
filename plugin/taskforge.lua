7d2c7e597d8ebd6d6a9e24f72c4dbd8405e7cb05
Christian C. Berclaz <christian.berclaz@mac.com>
2025-01-31 17:57:25 +0100

    fix: corrected the wrong ChatGPT configuration

--- plugin/taskforge.lua
+++ plugin/taskforge.lua
@@ -1,15 +1,18 @@
 require("taskforge")
 
+local subcommands = require("taskforge.subcommands")
+vim.api.nvim_create_user_command(
+  "TaskForge",
+  subcommands.run,
+  { nargs = 1, bang = true, complete = subcommands.complete, desc = "Run Task Forge command" }
+)
 
 -- if not vim.fn.has('nvim-0.7.0') then
 --   require('session_manager.utils').notify('Neovim 0.7+ is required for session manager plugin', vim.log.levels.ERROR)
 --   return
 -- end
 
-local subcommands = require('taskforge.subcommands')
 -- local taskforge = require('taskforge')
-
-vim.api.nvim_create_user_command('TaskForge', subcommands.run, { nargs = 1, bang = true, complete = subcommands.complete, desc = 'Run Task Forge command' })
 
 -- local taskforge_group = vim.api.nvim_create_augroup('TaskForge', {})
 -- vim.api.nvim_create_autocmd({ 'VimEnter' }, {
